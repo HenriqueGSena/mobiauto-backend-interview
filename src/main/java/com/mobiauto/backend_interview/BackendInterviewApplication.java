7953def1fa57c9849dab70f348caeb185b8ab1eb
henrique.sena <carloshenrique3250@gmail.com>
2024-05-24 20:13:57 -0300

    main
    - Gerando projeto spring

---
+++ src/main/java/com/mobiauto/backend_interview/BackendInterviewApplication.java
@@ -0,0 +1,13 @@
+package com.mobiauto.backend_interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendInterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mobiauto/backend_interview/BackendInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobiauto.backend_interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendInterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
