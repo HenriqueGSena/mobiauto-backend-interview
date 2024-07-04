9a69116d796de6a1d5ee80eaf827990c232ee070
henrique.sena <carloshenrique3250@gmail.com>
2024-07-04 20:54:02 -0300

    mob-dev-v1.3.1
    * Corrigindo erro na coluna da tabela Veiculos;
    * Ajuste de seguranca para url de veiculos;
    * Criando service para entidade veiculos;
    * Modificando VeiculosDTO e VeiculosController;

--- src/main/java/com/mobiauto/backend_interview/controller/VeiculosController.java
+++ src/main/java/com/mobiauto/backend_interview/controller/VeiculosController.java
@@ -3,13 +3,13 @@ package com.mobiauto.backend_interview.controller;
 import com.mobiauto.backend_interview.dto.VeiculosDTO;
 import com.mobiauto.backend_interview.entities.Veiculos;
 import com.mobiauto.backend_interview.repository.VeiculosRepository;
+import com.mobiauto.backend_interview.service.VeiculosService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/veiculos")
@@ -18,10 +18,18 @@ public class VeiculosController {
     @Autowired
     private VeiculosRepository veiculosRepository;
 
+    @Autowired
+    private VeiculosService veiculosService;
+
     @PostMapping("/criar")
     public ResponseEntity criarVeiculo(@RequestBody @Valid VeiculosDTO body) {
         Veiculos newVeiculos = new Veiculos(body);
         this.veiculosRepository.save(newVeiculos);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<VeiculosDTO>> listarVeiculos() {
+        return ResponseEntity.ok(this.veiculosService.listarVeiculos());
     }
 }
